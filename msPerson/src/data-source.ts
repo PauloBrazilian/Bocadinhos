2bc05c6cfa8aeb1200fe715051887f2101e765e3
Matheus Henrique Bonfim da Silva <104633582+RexDetona@users.noreply.github.com>
2024-08-29 11:59:34 -0300

    feat: new structure of msPerson
    Nova estrutura criada no projeto, inicio

---
+++ msPerson/src/data-source.ts
@@ -0,0 +1,24 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "test",
+    password: "test",
+    database: "test",
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+})
+
+
+AppDataSource.initialize()
+    .then(() => {
+        // here you can start to work with your database
+    })
+    .catch((error) => console.log(error))
