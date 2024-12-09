75e96ae2bcb348bae8685af6d28cf4a7aad627d7
Paulo Ricardo <paulo.rdndc@gmail.com>
2024-12-09 19:17:49 -0300

    Feat: Add BuyRouter e Adjust HistoryRouter

--- msorder/src/routes/HistoryRoutes.ts
+++ msorder/src/routes/HistoryRoutes.ts
@@ -6,8 +6,8 @@ const historyController = new HistoryController();
 
 router.post("/", (req, res) => historyController.createHistory(req, res));
 router.get("/", (req, res) => historyController.findAllHistories(req, res));
-router.get("/{id}", (req, res) => historyController.findHistoryById(req, res));
-router.put("/{id}", (req, res) => historyController.updateHistory(req, res));
-router.delete("/{id}", (req, res) => historyController.deleteHistory(req, res));
+router.get("/:id", (req, res) => historyController.findHistoryById(req, res));
+router.put("/:id", (req, res) => historyController.updateHistory(req, res));
+router.delete("/:id", (req, res) => historyController.deleteHistory(req, res));
 
 export default router;
